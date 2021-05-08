4d9c69f625befe66c1bc08f63e972e2027afbfca
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-05-08 19:13:18 +0900

    사용자 정의 타입 가드

---
+++ api/api.ts
@@ -0,0 +1,16 @@
+Array;
+
+class Animal {}
+class Cat extends Animal {
+  isCat: boolean = true;
+}
+class Dog extends Animal {
+  isDog: boolean = true;
+}
+const animals: Animal[] = [new Cat(), new Cat(), new Dog()];
+function isCat(animal: Animal): animal is Cat {
+  return (animal as Cat).isCat !== undefined;
+}
+
+console.log(animals.every<Cat>(isCat));
+
