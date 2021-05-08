f98467b7301f201b7abda10c37ff1e77b387b8d8
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-05-08 19:13:30 +0900

    Exception 처리

---
+++ exception/exception.ts
@@ -0,0 +1,31 @@
+{// Java: Exception
+// JavaScript: Error
+
+// Error(Exception) Handling: try -> catch -> finally
+
+function readFile(fileName: string): string {
+  if (fileName === 'not exist!') {
+    throw new Error(`file not exist! ${fileName}`);
+  }
+
+  return 'file contents';
+}
+
+function closeFile(fileName: string) {
+  //
+}
+function run() {
+const fileName = 'not exist!';
+
+  try {
+    console.log(readFile(fileName));
+  } catch (error) {
+    console.log('catched!!');
+  } finally {
+    closeFile(fileName);
+    console.log(`finally`);  
+  }
+}
+
+run();
+}
